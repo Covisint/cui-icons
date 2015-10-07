5de12f7ad5b7b17eb9f33837298d661a6734114b
Ricardo Marques <ricardo.marques@rocklobstah.local>
2015-10-07 10:29:03 -0500

    adds gzip function to generateSvgList.js

--- generateSvgList.js
+++ generateSvgList.js
@@ -1,4 +1,12 @@
 var fs = require('fs');
+var zlib = require('zlib');
 
+//creates svgList file
 var files=fs.readdirSync('src/resources/svg');
 fs.writeFileSync('svgList',files);
+
+//gzips the outputsvg
+var gzip = zlib.createGzip();
+var inp = fs.createReadStream('dist/svg/svg-out.svg');
+var out = fs.createWriteStream('dist/svg/svg-out.svg.gz');
+inp.pipe(gzip).pipe(out);
